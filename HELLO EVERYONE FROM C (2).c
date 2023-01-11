a38983a4cec170854dde7aec2f08c6bf5f09fc32
zebecks <122075086+zebecks@users.noreply.github.com>
2023-01-11 08:29:36 +0700

    Add files via upload

---
+++ HELLO EVERYONE FROM C (2).c
@@ -0,0 +1,14 @@
+/* simplePrograml.c
+   Demo. for a simple c profram
+   Author: Bac
+   Date: 2023/1/6
+*/     
+#include<stdio.h>
+int main(){
+	/* Print out a greeting to the screen*/
+	printf("Hello everyone from C");
+	/* wait use pressing the ENTER key */
+	getchar();
+	/* Quit the program */
+	return 0;
+}
